0143ac594a8292c9d6be52111a9fbf9f21c16a7e
kumarsonu729265-debug <kumarsonu729265@gmail.com>
2026-06-10 12:20:47 +0000

    config done

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,15 @@
+let a = 20;
+const b = 3.14;
+console.log(typeof a);
+
+
+let name = "sonu"
+
+let isloggedin = false
+
+let user;
+
+let x = null
+
+let a=1548522591536553n
+
--- 01_Basics/variables.js
+++ 01_Basics/variables.js
@@ -1,1 +1,15 @@
+var name = "sonu";
+var name = "monu"
+name = " rohan"
+
+
+let age=20
+age=30
+
+
+const a=30
+
+
+console.table([name, age , a])
+
 
