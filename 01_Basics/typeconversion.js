7d721b2fdfeefcf3f356d113b12aa9a44b5d490e
kumarsonu729265-debug <kumarsonu729265@gmail.com>
2026-06-22 08:14:08 +0000

    type conv.

---
+++ 01_Basics/typeconversion.js
@@ -0,0 +1,12 @@
+let x ="88";
+inNumber = Number(x);
+
+console.log(typeof x)
+console.log(inNumber)
+console.log(typeof inNumber)
+
+let y="XY"
+p = Number(y)
+console.log(typeof y)
+console.log(p)
+console.log(typeof p);
